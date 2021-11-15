e02aa3e74e6f6b49566cb3ea2aa140fe705e6775
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-11-15 21:59:02 +0300

    add classes need for the application

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,2 +1,38 @@
-package models;public class Event {
+package models;
+
+public class Event {
+
+    private String title;
+    private String location;
+    private String time;
+    private int price;
+    private String host;
+
+    public Event(String title, String location, String time, int price, String host) {
+        this.title = title;
+        this.location = location;
+        this.time = time;
+        this.price = price;
+        this.host = host;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getHost() {
+        return host;
+    }
 }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,25 @@
 package models;
 
 public class User {
+    private String name;
+    private String phoneNumber;
+    private String ticket;
+
+    public User(String name, String phoneNumber, String ticket) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.ticket = ticket;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getTicket() {
+        return ticket;
+    }
 }
