0420fb7b37f10985d47ea5e4488b7b279c523e00
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-11-15 23:19:06 +0300

    add the main fuctionality of the application

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,38 @@
+import models.Event;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        get("/",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            return new ModelAndView(model,"index.hbs");
+        },new HandlebarsTemplateEngine());
+
+        //Event
+        get("/events/new",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            return new ModelAndView(model,"eventform.hbs");
+        },new HandlebarsTemplateEngine());
+
+        post("/events/new",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            String title=request.queryParams("title");
+            String location=request.queryParams("location");
+            Integer price=Integer.parseInt(request.queryParams("price"));
+            String time=request.queryParams("time");
+            String host=request.queryParams("host");
+            Event ranger=new Event(title, location, time, price, host);
+            ranger.save();
+            System.out.println(ranger);
+            model.put("ranger",ranger);
+
+            return new ModelAndView(model,"success.hbs");
+        },new HandlebarsTemplateEngine());
+    }
 }
