631099f187f92b9a07756dc7ba02234c7ece2251
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-11-15 23:18:29 +0300

    add the neccessary classes for the application

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,19 +1,26 @@
 package models;
 
+import org.sql2o.Connection;
+
 public class Event {
 
     private String title;
     private String location;
     private String time;
-    private int price;
+    private Integer price;
     private String host;
+    private int id;
 
-    public Event(String title, String location, String time, int price, String host) {
+    public Event(String title, String location, String time, Integer price, String host) {
         this.title = title;
         this.location = location;
         this.time = time;
         this.price = price;
         this.host = host;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
@@ -28,11 +35,25 @@ public class Event {
         return time;
     }
 
-    public int getPrice() {
+    public Integer getPrice() {
         return price;
     }
 
     public String getHost() {
         return host;
+    }
+
+    public void save() {
+        Connection con=DB.sql2o.open();
+            String sql="INSERT INTO event (title, location, time, price, host) VALUES (:name,:location,:time,:price,:host)";
+            this.id=(int) ((Connection) con).createQuery(sql,true)
+                    .addParameter("title",this.title)
+                    .addParameter("location",this.location)
+                    .addParameter("time",this.location)
+                    .addParameter("price",this.price)
+                    .addParameter("host",this.host)
+                    .executeUpdate()
+                    .getKey();
+
     }
 }
